453140009739f5c4c60e402961da624b6058ba92
hakyeongchoi1217 <lesser1217@naver.com>
2025-09-03 05:24:41 +0900

    feat(backend): init spring boot

---
+++ backend/src/test/kotlin/com/github/dswubackdoor/kucis/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dswubackdoor.kucis.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
